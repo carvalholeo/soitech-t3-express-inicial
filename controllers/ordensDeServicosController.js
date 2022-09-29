68a843257853eca80dd7bd2584158b036e0e981f
Léo Carvalho <carvalho.csleo@gmail.com>
2022-09-28 21:33:11 -0300

    Cria mais rotas de ordens de serviço Cria controllers para rotas de ordens de serviço

--- controllers/ordensDeServicosController.js
+++ controllers/ordensDeServicosController.js
@@ -1,6 +1,15 @@
 const ordensDeServicosController = {
   base: (req, res) => {
     return res.json({mensagem: 'Esta é uma ordem de serviço'});
+  },
+  cadastrar: (req, res) => {
+    return res.json({mensagem: 'ordem de serviço cadastrada'})
+  },
+  atualizar: (req, res) => {
+    return res.json({mensagem: 'ordem de serviço atualizada'});
+  },
+  delete: (req, res) => {
+    return res.json({mensagem: "Ordem de serviço excluída"});
   }
 }
 
--- routes/ordensDeServicos.js
+++ routes/ordensDeServicos.js
@@ -6,4 +6,12 @@ const route = Router();
 
 route.get('/', ordensDeServicosController.base);
 
+//rotas a criar: excluir
+
+route.post('/', ordensDeServicosController.cadastrar);
+
+route.patch('/', ordensDeServicosController.atualizar);
+
+route.delete("/", ordensDeServicosController.delete);
+
 module.exports = route;
