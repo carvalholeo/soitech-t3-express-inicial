7e55ba454831a40f8bedd321b0073bf49082bb1f
Léo Carvalho <carvalho.csleo@gmail.com>
2022-10-03 21:43:05 -0300

    Limpa arquivo index.js Envia status correto para o usuário em caso de manutenção

--- index.js
+++ index.js
@@ -1,38 +1,14 @@
 const express = require('express');
 
 const routes = require('./routes/index');
+const modoManutencao = require('./middlewares/modoManutencao');
 
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use(function(req, res, next){
-  console.log('Sistema ativado com sucesso');
-  next();
-});
-
-const estaDeslogado = false;
-
-app.use((req, res, next) => {
-  if (estaDeslogado) {
-    return res.json('Este usuário não fez login no sistema');
-  }
-  next();
-});
-
-const modoManutencao = true;
-app.use((req, res, next) => {
-  if (modoManutencao) {
-    return res.json('Oops, sistema em manutenção. Vá ler um livro.');
-  }
-  next();
-});
-
-// Criar uma constante para identificar se o sistema está ou não em manutenção - OK
-// Criar um middleware global que verifica essa constante
-// Cortar a requisição se o sistema estiver em manutenção
-// Avançar a requisição caso o sistema esteja operante
+app.use(modoManutencao);
 
 app.use(routes);
 
--- middlewares/modoManutencao.js
+++ middlewares/modoManutencao.js
@@ -2,7 +2,9 @@ const modoManutencao = false;
 
 function modoManutencaoMiddleware(req, res, next) {
   if (modoManutencao) {
-    return res.json('Oops, sistema em manutenção. Vá ler um livro.');
+    return res
+      .status(503)
+      .json('Oops, sistema em manutenção. Vá ler um livro.');
   }
   next();
 }
