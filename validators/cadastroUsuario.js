100cbc68702b4474b62a6a0445f6410920c23dab
Léo Carvalho <carvalho.csleo@gmail.com>
2022-12-05 21:58:42 -0300

    Cria validações para cpf, email e usuários unicos

--- validators/cadastroUsuario.js
+++ validators/cadastroUsuario.js
@@ -1,4 +1,5 @@
 const { check } = require('express-validator');
+const bcrypt = require('bcrypt');
 const {Usuario} = require('../database/repository');
 
 // nome, cpf, data de nascimento, e-mail, telefone, senha, usuário
@@ -6,7 +7,19 @@ const arrayDeValidacao = [
   check('email')
     .notEmpty().withMessage('Campo e-mail não pode estar vazio')
     .isEmail().withMessage('E-mail não é válido')
-    .isLength({ max: 50 }).withMessage('Campo recebe no máximo 50 caracteres'),
+    .isLength({ max: 50 }).withMessage('Campo recebe no máximo 50 caracteres')
+    .custom(async (field) => {
+      const usuario = await Usuario.findOne({
+        where: {
+          email: field
+        }
+      });
+
+      if (usuario) {
+        throw "Email já existe no sistema";
+      }
+      return true;
+    }),
   check('senha')
     .notEmpty().withMessage('Senha não pode ser vazia')
     .isStrongPassword({
@@ -15,9 +28,38 @@ const arrayDeValidacao = [
     minUppercase: 2,
     minNumbers: 2,
     minSymbols: 1
-    }).withMessage('Sua senhas precisa ter no mínimo 8 caracteres, sendo 2 letras minúsculas, 2 maiúsculas, 2 números e 1 caractere especial'),
+    }).withMessage('Sua senhas precisa ter no mínimo 8 caracteres, sendo 2 letras minúsculas, 2 maiúsculas, 2 números e 1 caractere especial')
+    // .custom(async (field, req) => {
+    //   const usuario = await Usuario.findOne({
+    //     where: {
+    //       usuario: req.body.usuario
+    //     }
+    //   });
+
+    //   if (usuario) {
+    //     const senhaValida = bcrypt.compareSync(field, usuario.senha);
+    //     if (senhaValida) {
+    //       throw "Nova senha precisa ser diferente da senha anterior."
+    //     }
+    //   }
+    //   return true;
+    // }),
   check('nome').notEmpty().isLength({max: 50, min: 4}),
-  check('cpf').notEmpty().isTaxID('pt-BR'),
+  check('cpf')
+    .notEmpty()
+    .isTaxID('pt-BR')
+    .custom(async (field) => {
+      const usuario = await Usuario.findOne({
+        where: {
+          cpf: field
+        }
+      });
+
+      if (usuario) {
+        throw "CPF já está cadastrado no sistema";
+      }
+      return true;
+    }),
   check('dataNascimento')
     .isDate().withMessage('Data de nascimento precisa estar no formato AAAA/MM/DD.')
     .notEmpty(),
