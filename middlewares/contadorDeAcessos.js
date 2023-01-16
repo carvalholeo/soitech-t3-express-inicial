c071b52ee3e73b4e1704f787506c751f6f6e7c54
Léo Carvalho <carvalho.csleo@gmail.com>
2023-01-16 19:50:02 -0300

    Troca contador em arquivo para contador em memória

--- middlewares/contadorDeAcessos.js
+++ middlewares/contadorDeAcessos.js
@@ -1,18 +1,9 @@
-const fs = require('fs');
-
-// pegar o arquivo contagemAtual.txt - OK
-// adicionar + 1 ao valor atual
-// salvar o arquivo contagemAtual.txt
-
+let contador = 0;
 
 function contadorDeAcessos(req, res, next) {
   try {
-    const arquivo = fs.readFileSync(__dirname + '/contagemAtual.txt', { encoding: 'utf8' });
-
-    let contador = parseInt(arquivo);
     contador++;
 
-    fs.writeFileSync(__dirname + '/contagemAtual.txt', String(contador));
 
     console.log('Quantidade de acessos registrados', contador);
     next();
