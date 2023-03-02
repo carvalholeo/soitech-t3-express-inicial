a319dcb4e25d08c81fb0210cc0f8200157335d44
Léo Carvalho <carvalho.csleo@gmail.com>
2023-03-01 21:18:50 -0300

    Altera variável da porta e ignora DB de teste

--- src/server.js
+++ src/server.js
@@ -1,3 +1,3 @@
 const app = require('./index');
 
-app.listen(process.env.PORTA || 8080, () => console.log('Express subiu o servidor'));
+app.listen(process.env.PORT || 8080, () => console.log('Express subiu o servidor'));
